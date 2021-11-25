8a2928f1d0c951fbbf48cae2e9ed540517042386
Paul Hinze <phinze@phinze.com>
2021-11-25 14:58:19 -0600

    tunnel: set hostname

--- machines/vm-tunnel.nix
+++ machines/vm-tunnel.nix
@@ -3,6 +3,8 @@
     ./vm-shared.nix
   ];
 
+  networking.hostName = pkgs.lib.mkForce "tunnel";
+
   services.xrdp.enable = true;
   services.xrdp.defaultWindowManager = "${pkgs.i3}/bin/i3";
   networking.firewall.allowedTCPPorts = [ 3389 ];
