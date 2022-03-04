7748f39af0782145d4cae056643b795277aeeca2
Paul Hinze <phinze@phinze.com>
2022-03-04 11:35:23 -0600

    Let's try an all-agent-forwarding lifestyle

--- users/phinze/config.fish
+++ users/phinze/config.fish
@@ -36,9 +36,9 @@ if test -z "$SSH_ENV"
     set -xg SSH_ENV $HOME/.ssh/environment
 end
 
-if not __ssh_agent_is_started
-    __ssh_agent_start
-end
+# if not __ssh_agent_is_started
+#     __ssh_agent_start
+# end
 
 #-------------------------------------------------------------------------------
 # Vim
