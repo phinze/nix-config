58ac6b45769d0993859360aa1ffaa1219cbf2cf9
Paul Hinze <phinze@phinze.com>
2022-07-29 11:37:05 -0500

    NFS server on hc-dev
    For reproing NFS vagrant things

--- machines/vm-hc-dev.nix
+++ machines/vm-hc-dev.nix
@@ -13,4 +13,16 @@
   # Virtualbox for Vagrant dev
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = ["phinze"];
+
+  # NFS for Vagrant dev
+  services.nfs.server = {
+    enable = true;
+    # fixed rpc.statd port; for firewall
+    lockdPort = 4001;
+    mountdPort = 4002;
+    statdPort = 4000;
+    extraNfsdConfig = '''';
+  };
+  networking.firewall.allowedTCPPorts = [ 2049 111 4000 4001 4002 ];
+  networking.firewall.allowedUDPPorts = [ 2049 111 4000 4001 4002 ];
 }
