6b5f2e645e413b354406c45b9c5cac0ab1d6acb9
Paul Hinze <phinze@phinze.com>
2023-10-27 11:09:59 -0500

    cattpuccin in ghostty too

--- users/phinze/home-manager.nix
+++ users/phinze/home-manager.nix
@@ -226,6 +226,39 @@ let sources = import ../../nix/sources.nix; in {
       font-family = Hack
       clipboard-read = true
       clipboard-write = true
+
+      ## Catppuccin Mocha Theme
+      background           = 1E1E2E
+      foreground           = CDD6F4
+      cursor-color         = F5E0DC
+      selection-background = F5E0DC
+      selection-foreground = 1E1E2E
+
+      # black
+      palette = 0=#45475A
+      palette = 8=#585B70
+      # red
+      palette = 1=#F38BA8
+      palette = 9=#F38BA8
+      # green
+      palette = 2=#A6E3A1
+      palette = 10=#A6E3A1
+      # yellow
+      palette = 3=#F9E2AF
+      palette = 11=#F9E2AF
+      # blue
+      palette = 4=#89B4FA
+      palette = 12=#89B4FA
+      # magenta
+      palette = 5=#F5C2E7
+      palette = 13=#F5C2E7
+      # cyan
+      palette = 6=#94E2D5
+      palette = 14=#94E2D5
+      # white
+      palette = 7=#BAC2DE
+      palette = 15=#A6ADC8
+      ## End Theme
     '';
   };
 
