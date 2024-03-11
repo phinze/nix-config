b98ed9840a7b6fcd9c221d50ec6382383fd9f9d0
Paul Hinze <phinze@phinze.com>
2024-03-11 11:58:54 -0500

    Update nix and flake

--- flake.nix
+++ flake.nix
@@ -5,9 +5,9 @@
     # Pin our primary nixpkgs repository. This is the main nixpkgs repository
     # we'll use for our configurations. Be very careful changing this because
     # it'll impact your entire system.
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
 
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # We have access to unstable nixpkgs if we want specific unstable packages.
