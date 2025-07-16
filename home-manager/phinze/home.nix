d62563d5dedd7e628ef56596822152350fa23904
Paul Hinze <phinze@phinze.com>
2025-07-16 11:46:28 -0500

    Catch tmux catppuccin config up to recent updates
    Tmux was looking funky after recent package updates, so needed to read
    docs and rebuild config

--- home-manager/phinze/home.nix
+++ home-manager/phinze/home.nix
@@ -324,8 +324,15 @@
       {
         plugin = catppuccin;
         extraConfig = ''
-          # Add hostname to statusbar
-          set -g @catppuccin_status_modules_right "application session host"
+          set -g @catppuccin_window_status_style "rounded"
+          set -g @catppuccin_window_flags "icon"
+
+          set -g status-right-length 100
+          set -g status-left-length 100
+          set -g status-left ""
+          set -g status-right "#{E:@catppuccin_status_application}"
+          set -ag status-right "#{E:@catppuccin_status_session}"
+          set -ag status-right "#{E:@catppuccin_status_host}"
         '';
       }
       {
