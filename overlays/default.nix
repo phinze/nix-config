1bc984a6b6a4676a8c4a701b6263b683b20b82eb
Paul Hinze <phinze@phinze.com>
2025-08-11 11:44:23 -0500

    Configure SSH signing with 1Password and conditional emails

--- overlays/default.nix
+++ overlays/default.nix
@@ -12,7 +12,8 @@
     # });
 
     # Apply Wayland fixes from https://github.com/NixOS/nixpkgs/pull/422792
-    _1password-gui = prev._1password-gui.overrideAttrs (oldAttrs: {
+    # Only on Linux since macOS uses homebrew cask for 1Password
+    _1password-gui = if prev.stdenv.isLinux then prev._1password-gui.overrideAttrs (oldAttrs: {
       installPhase =
         oldAttrs.installPhase
         + ''
@@ -21,7 +22,7 @@
             --replace "Exec=1password %U" \
             "Exec=1password --ozone-platform-hint=auto %U"
         '';
-    });
+    }) else prev._1password-gui;
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
