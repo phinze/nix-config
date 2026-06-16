846d3dc503bb92f6c984600deb28b7682ef5a1e9
Paul Hinze <phinze@phinze.com>
2026-06-16 13:34:32 -0500

    update antigravity-cli: 1.0.0 -> 1.0.9

--- pkgs/antigravity-cli/default.nix
+++ pkgs/antigravity-cli/default.nix
@@ -5,25 +5,25 @@
   autoPatchelfHook,
 }:
 let
-  version = "1.0.0";
-  buildId = "1.0.0-5288553236791296";
+  version = "1.0.9";
+  buildId = "1.0.9-6422111942737920";
 
   sources = {
     x86_64-linux = {
       url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/${buildId}/linux-x64/cli_linux_x64.tar.gz";
-      sha256 = "sha256-cAljQFdPr8SgbE08gFcxTiLUdc4cgg0K1R/wf7fpnrY=";
+      sha256 = "sha256-Qzx5KWlAd1amFImSYYg6jhMPD6+fQwIGcBXgNImDvUY=";
     };
     aarch64-linux = {
       url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/${buildId}/linux-arm/cli_linux_arm64.tar.gz";
-      sha256 = "sha256-9Nx8lsGDawB2jYpuxurMeFHzQkvW9Ovk2LhIplIHKoU=";
+      sha256 = "sha256-VLCDOyNK8TwJCafdN5BSC60Uenf+gm+uyFYPLgsEpBc=";
     };
     x86_64-darwin = {
       url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/${buildId}/darwin-x64/cli_mac_x64.tar.gz";
-      sha256 = "sha256-dEoaJdzwv2Yo463XZNIVXETX0XTt+LGBp0J/fZ+z+1M=";
+      sha256 = "sha256-zvDp0yerDpIdLSRN72YTGfApMSO/NhFSuOChFMCaCng=";
     };
     aarch64-darwin = {
       url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/${buildId}/darwin-arm/cli_mac_arm64.tar.gz";
-      sha256 = "sha256-ZcL3teJ6Ie+YOxYe11hm6JE5poKt9nkADhpdnTdOMgo=";
+      sha256 = "sha256-sOmQPQr4IfwKltc/u1H23aCBD5UgAoZinZVrOH8/jIE=";
     };
   };
 
